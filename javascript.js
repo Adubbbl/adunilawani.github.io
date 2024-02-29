22c2a26db47aa3e773ca0df2285e9622d4db8f16
Aduni Lawani <lawaniaduni75@gmail.com>
2024-02-29 12:57:12 +0100

    responsive nav

--- javascript.js
+++ javascript.js
@@ -1,9 +1,18 @@
-window.addEventListener('scroll', function() {
-    var header = document.querySelector('header');
+// window.addEventListener('scroll', function() {
+//     var header = document.querySelector('header');
 
-    if (this.window.scrollY > 0){
-        header.classList.add('onScroll_h');
-    } else{
-        header.classList.remove('onScroll_h');
+//     if (this.window.scrollY > 0){
+//         header.classList.add('onScroll_h');
+//     } else{
+//         header.classList.remove('onScroll_h');
+//     }
+// });
+
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
     }
-});
+  }
